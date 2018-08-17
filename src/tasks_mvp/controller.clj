67cabdb7143e194cf227b40c09c71baccd91becc
gabrielgiov <gabrielg.desouza@gmail.com>
2018-08-17 16:16:10 +0000

    Parse params

--- src/tasks_mvp/controller.clj
+++ src/tasks_mvp/controller.clj
@@ -1,3 +1,7 @@
 (ns tasks-mvp.controller)
 
-(defn add [params])
+(defn add [params]
+  (println params))
+
+(defn ls [params]
+  (println params))
--- src/tasks_mvp/core.clj
+++ src/tasks_mvp/core.clj
@@ -1,13 +1,21 @@
 (ns tasks-mvp.core
-  (:require [tasks-mvp.controller :as c])
+  (:require [tasks-mvp.controller :as c]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 (def add-option
   [["-m" "--message MESSAGE" "Task description"]])
 
-(defn route [action & params]
-  (case action
-    "add" (c/add params)))
+(def ls-option
+  [["-t" "--today" "Show all task for today"]])
+
+(defn route [params]
+  (let [[action & ops] params]
+    (case action
+      "add" (let [o (parse-opts ops add-option)]
+              (c/add (:options o)))
+      "ls" (let [o (parse-opts ops ls-option)]
+             (c/ls (:options o))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
